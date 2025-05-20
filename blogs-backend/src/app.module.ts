4b8ce09230e94e9dd3012db3561cf23f94bcaea7
lzh <lizihao@kqgyl.com>
2025-05-20 09:22:20 +0800

    feat: 添加NestJS Throttler模块以限制请求频率
    在应用中引入@nestjs/throttler模块，配置每秒最多10次请求的限制，以防止滥用和确保系统稳定性。

--- blogs-backend/src/app.module.ts
+++ blogs-backend/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostModule } from './post/post.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [PrismaModule, PostModule],
+  imports: [PrismaModule, PostModule, ThrottlerModule.forRoot({
+    throttlers:[{
+      ttl: 60,
+      limit: 10
+    }]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
